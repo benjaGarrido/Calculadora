61fd523aa53afbf11267ccd09bc617374ced25ea
Benjamín Garrido Barreiro <benjamin.garrido@gmail.com>
2016-12-10 19:39:26 +0100

    bgarrido - adding actions to buttons

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -8,16 +8,67 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum CalcOp : String {
+    case Add = "op-suma"
+    case Substract = "op-resta"
+    case Multiply = "op-multiplica"
+    case Divide = "op-divide"
+}
 
+class ViewController: UIViewController {
+    @IBOutlet weak var icoSuma: UIImageView!
+    @IBOutlet weak var icoDividir: UIImageView!
+    @IBOutlet weak var icoMultiplica: UIImageView!
+    @IBOutlet weak var icoResta: UIImageView!
+    @IBOutlet weak var lblResult: UILabel!
+    
+    let currencyFormatter = NumberFormatter()
+    
+    var result : String? {
+        didSet {
+            if let result = result {
+                lblResult.text = currencyFormatter.string(from: NSNumber(value: Double(result.replacingOccurrences(of: ",", with: "."))!))
+            }
+        }
+    }
+    var previousResult : String?
+    var currentOperation : CalcOp? {
+        willSet {
+            resetIcons()
+        }
+        didSet {
+            if let currentOperation = currentOperation {
+                switch currentOperation {
+                case .Add:
+                    icoSuma.image = UIImage(named: "\(CalcOp.Add.rawValue)-on")
+                case .Substract:
+                    icoResta.image = UIImage(named: "\(CalcOp.Substract.rawValue)-on")
+                case .Multiply:
+                    icoMultiplica.image = UIImage(named: "\(CalcOp.Multiply.rawValue)-on")
+                case .Divide:
+                    icoDividir.image = UIImage(named: "\(CalcOp.Divide.rawValue)-on")
+                }
+            } else {
+                resetIcons()
+            }
+        }
+    }
+    var previousOperation : CalcOp?
+    
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func resetIcons() {
+        icoSuma.image = UIImage(named: CalcOp.Add.rawValue)
+        icoResta.image = UIImage(named: CalcOp.Substract.rawValue)
+        icoMultiplica.image = UIImage(named: CalcOp.Multiply.rawValue)
+        icoDividir.image = UIImage(named: CalcOp.Divide.rawValue)
     }
     
     override var prefersStatusBarHidden: Bool {
